14ef8f538628d55f8494852d5ac8706cabf9afc1
leninner <mazabandalenin180@gmail.com>
2022-07-11 00:32:43 -0500

    Update the menu logic

--- src/App.java
+++ src/App.java
@@ -6,20 +6,21 @@ public class App {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int option;
+        System.out.println(
+                "\n\nBienvenido al Proyecto del Segundo Parcial de Algoritmos y Lógica de Programación\n\n");
+        System.out.println("Elije una opción para empezar\n");
 
         do {
-            System.out.println("\n\n\n");
-            System.out.println("Welcome to the Ticket Management System!");
-            System.out.println("Please select an option:");
-            System.out.println("1. Tickets Selling");
-            System.out.println("2. Files Management");
-            System.out.println("3. Exit");
-            System.out.print("Option: ");
+            System.out.println("\t1. Tickets Selling");
+            System.out.println("\t2. Files Management");
+            System.out.println("\t3. Exit");
+            System.out.print("\n\tOption -> ");
 
             try {
                 option = Integer.valueOf(scan.next().replaceAll("\\D*", ""));
             } catch (Exception e) {
-                System.out.println("Invalid option!");
+                System.out.println("\n\nProcura no cometer errores al ingresar la opción.");
+                System.out.println("Debes ingresar un número entre 1 y 3.\n\n");
                 option = 0;
                 scan.nextLine();
             }
@@ -32,10 +33,10 @@ public class App {
                     FilesManagement.main(null);
                     break;
                 case 3:
-                    System.out.println("Goodbye!");
+                    System.out.println("\nEs un gusto tenerte en nuestra aplicación, hasta pronto!\n");
                     break;
                 default:
-                    System.out.println("Invalid option!");
+                    System.out.println("\nEsa opción no existe, intenta de nuevo\n");
                     break;
             }
         } while (option != 3);
