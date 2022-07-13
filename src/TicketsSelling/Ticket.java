6b88ba8610f099b8c2b11689e85289ae8e1868d1
Emi1213 <emi1213galarza@gmail.com>
2022-07-13 15:15:55 -0500

    Inicializacion de tickets

--- src/TicketsSelling/Ticket.java
+++ src/TicketsSelling/Ticket.java
@@ -1,8 +1,34 @@
 package TicketsSelling;
 
+import java.util.Scanner;
+
 public class Ticket {
-    public static void main(String[] args) {
-        System.out.println("Funciona");
-        System.out.println("Vlemos vrg");
-    }
+  public static Scanner scan = new Scanner(System.in);
+
+  public static void main(String[] args) {
+
+    boolean exit = false;
+    boolean validexit = false;
+
+    do {
+      Credentials credentials = new Credentials();
+      credentials.getCredentials();
+
+      do {
+        System.out.println("¿Desea ejecutar esta opción otra vez? (si/no)");
+        String answer = scan.nextLine();
+
+        if (answer.equalsIgnoreCase("si")) {
+          validexit = true;
+        } else if (answer.equalsIgnoreCase("no")) {
+          validexit = true;
+          exit = true;
+        } else {
+          System.out.println("Respuesta inválida, intente de nuevo");
+        }
+
+      } while (!validexit);
+    } while (!exit);
+    scan.close();
+  }
 }
