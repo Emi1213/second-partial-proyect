db853419a3f8c001f75c4ce78cb0cc1cbe5382c1
Emi1213 <emi1213galarza@gmail.com>
2022-07-13 14:06:50 -0500

    Creacion de rama para la opcion 2

--- src/TicketsSelling/Ticket.java
+++ src/TicketsSelling/Ticket.java
@@ -3,5 +3,6 @@ package TicketsSelling;
 public class Ticket {
     public static void main(String[] args) {
         System.out.println("Funciona");
+        System.out.println("Vlemos vrg");
     }
 }
