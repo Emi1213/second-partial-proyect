fc4c2717ff4def417dbff4b44b4e2f5ed0544068
Jaesbyp <96183427+Jaesbyp@users.noreply.github.com>
2022-07-16 22:29:10 -0500

    Fix Methods of FileCreation

--- src/FilesManagement/FilesManagement.java
+++ src/FilesManagement/FilesManagement.java
@@ -2,6 +2,22 @@ package FilesManagement;
 
 public class FilesManagement {
     public static void main(String[] args) {
+        String regexAnswer = "[]";
+        String answer = "";
+        String isValidAnwer = "";
+        System.out.println("Bienvenido a la opción para gestionar archivos y directorios");
+        do{
+        System.out.println("Vamos a empezar creando tu proyecto");
+        System.out.println("Estas de acuerdo? (si/no)");
+        answer = System.console().readLine().toLowerCase();
+        if (answer == "si"|| answer == "no"||answer == "s"||answer == "n") {
+            isValidAnwer = "si";
+        } else {
+            System.out.println("La respuesta ingresada no es válida");
+            isValidAnwer = "no";
+        }
+
+        }
 
     }
 }
